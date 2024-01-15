c6df2f367260de8439115908b74afca73f2aacbe
anish-oml <anish.saxenaa@gmail.com>
2024-01-16 04:36:49 +0530

    set up netlify

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,12 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true,
     },
-    lib: {
-      entry: "./src/main.tsx", // Specifies the entry point for building the library.
-      name: "gooey-chat", // Sets the name of the generated library.
-      fileName: (format) => `shim.${format}.js`, // Generates the output file name based on the format.
-      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
-    },
+    // lib: {
+    //   entry: "./src/main.tsx", // Specifies the entry point for building the library.
+    //   name: "gooey-chat", // Sets the name of the generated library.
+    //   fileName: (format) => `.${format}.js`, // Generates the output file name based on the format.
+    //   formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+    // },
     rollupOptions: {
       external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
       output: {
